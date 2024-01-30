b01b63698daae37a8fb515766f8ee2cfc467e17d
Uri-Raz-Dev <uri1515@gmail.com>
2024-01-30 20:19:24 +0200

    Finalize ball increment

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 'use strict'
 
 function onBallClick(ball) {
- let ballHeight = ball.offsetHeight
- let ballWidth = ball.offsetWidth
- ballHeight += 50;
- ballWidth += 50;
+ var ballHeight = ball.offsetHeight
+ var ballWidth = ball.offsetWidth
+ var ballSize = ballHeight + ballWidth + 100
+
+ if (ballSize <= 400) {
+
+  ballHeight += 50
+  ballWidth += 50
+ } else if (ballSize > 400) {
+  ballHeight = 100
+  ballWidth = 100
+  ballSize = 200
+ }
 
  ball.style.height = ballHeight + 'px'
  ball.style.width = ballWidth + 'px'
- ball.innerText = ballHeight + ballWidth
+ ball.innerText = ballSize
 }
