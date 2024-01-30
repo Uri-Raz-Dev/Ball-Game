00843b442b425adf5afdad4c03f2f9a3e1d2113d
Uri-Raz-Dev <147313459+Uri-Raz-Dev@users.noreply.github.com>
2024-01-30 22:41:35 +0200

    Increment the ball with random values

--- main.js
+++ main.js
@@ -3,19 +3,20 @@
 function onBallClick(ball) {
  var ballHeight = ball.offsetHeight
  var ballWidth = ball.offsetWidth
- var ballSize = ballHeight + ballWidth + 100
-
+ var ballSize = ballHeight + ballWidth
  if (ballSize <= 400) {
 
-  ballHeight += 50
-  ballWidth += 50
+  ballHeight += getRandomInt(20, 60)
+  ballWidth += getRandomInt(20, 60)
  } else if (ballSize > 400) {
   ballHeight = 100
   ballWidth = 100
   ballSize = 200
  }
+ ballSize = ballHeight + ballWidth
 
  ball.style.height = ballHeight + 'px'
  ball.style.width = ballWidth + 'px'
  ball.innerText = ballSize
+
 }
