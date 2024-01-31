bec47f643f1556f579a72c92aafcd68cc90c0981
Uri-Raz-Dev <147313459+Uri-Raz-Dev@users.noreply.github.com>
2024-01-31 14:38:49 +0200

    Create third ball swap ball1-2 styles

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ function onBallClick(ball) {
  var ballWidth = ball.offsetWidth
  var ballSize = ballHeight + ballWidth
  const maxDiameter = 400
- if (ballSize <= maxDiameter) {
+ const minDiameter = 100
+ if (ballSize <= maxDiameter || ballSize >= minDiameter) {
 
   ballHeight += getRandomInt(20, 60)
   ballWidth += getRandomInt(20, 60)
@@ -21,4 +22,18 @@ function onBallClick(ball) {
  ball.style.width = ballWidth + 'px'
  ball.innerText = ballSize
  ball.style.backgroundColor = getRandomColor()
+}
+
+function swapOnClick() {
+ const ball1 = document.querySelector('.ball1')
+ const ball2 = document.querySelector('.ball2')
+ const tempColor = ball1.style.backgroundColor
+ const tempHeight = ball1.style.height
+ const tempWidth = ball1.style.width
+ ball1.style.backgroundColor = ball2.style.backgroundColor
+ ball2.style.backgroundColor = tempColor
+ ball1.style.height = ball2.style.height
+ ball1.style.width = ball2.style.width
+ ball2.style.height = tempHeight
+ ball2.style.width = tempWidth
 }
