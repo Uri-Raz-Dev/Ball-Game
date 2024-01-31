df7b9ed0c672fc66d83d574c471435848dd4c03a
Uri-Raz-Dev <147313459+Uri-Raz-Dev@users.noreply.github.com>
2024-01-31 15:57:01 +0200

    Create ball 4 reduce ball

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 'use strict'
-
 function onBallClick(ball) {
  var ballHeight = ball.offsetHeight
  var ballWidth = ball.offsetWidth
  var ballSize = ballHeight + ballWidth
  const maxDiameter = 400
- const minDiameter = 100
- if (ballSize <= maxDiameter || ballSize >= minDiameter) {
+ if (ballSize <= maxDiameter) {
 
   ballHeight += getRandomInt(20, 60)
   ballWidth += getRandomInt(20, 60)
@@ -36,4 +34,43 @@ function swapOnClick() {
  ball1.style.width = ball2.style.width
  ball2.style.height = tempHeight
  ball2.style.width = tempWidth
+ ball1.innerText = parseInt(ball1.style.height, 10) + parseInt(ball1.style.width, 10)
+ ball2.innerText = parseInt(ball2.style.height, 10) + parseInt(ball2.style.width, 10)
+}
+
+function reduceOnClick() {
+ const ball1 = document.querySelector('.ball1')
+ const ball2 = document.querySelector('.ball2')
+
+ var ball1Height = ball1.offsetHeight
+ var ball2Height = ball2.offsetHeight
+ var ball1Width = ball1.offsetWidth
+ var ball2Width = ball2.offsetWidth
+
+ var ball1size = ball1Height + ball1Width
+ var ball2size = ball2Height + ball2Width
+
+
+ if (ball1size >= 100 && ball2size >= 100) {
+  ball1Height -= getRandomInt(20, 60)
+  ball2Height -= getRandomInt(20, 60)
+  ball1Width -= getRandomInt(20, 60)
+  ball2Width -= getRandomInt(20, 60)
+
+ } else if (ball1size < 100 || ball2size < 100) {
+  ball1Height = 50
+  ball1Width = 50
+  ball2Height = 50
+  ball2Width = 50
+  ball1size = 100
+  ball2size = 100
+ }
+ ball1.style.height = ball1Height + 'px'
+ ball1.style.width = ball1Width + 'px'
+ ball2.style.height = ball2Height + 'px'
+ ball2.style.width = ball2Width + 'px'
+ ball1size = ball1Height + ball1Width
+ ball2size = ball2Height + ball2Width
+ ball1.innerText = ball1size
+ ball2.innerText = ball2size
 }
